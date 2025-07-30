e4d77d6cdd8fdb607b22c3a279dd3008331b3424
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-07-30 23:00:39 +0200

    feat(layout): add NavHeaderLayout and style test

--- src/layouts/HeaderLayout.jsx
+++ src/layouts/HeaderLayout.jsx
@@ -1,10 +1,12 @@
 import styles from "../assets/styles/layouts/headerLayout.module.scss";
 import TitleMain from "../components/TitleMain.jsx";
+import NavHeaderLayout from "./NavHeaderLayout.jsx";
 
 export default function HeaderLayout() { 
     return (
         <header className={styles.headerLayout}>
             <TitleMain title="ColorBOX" showText={ true } text="by JulienLOG"/>
+            <NavHeaderLayout />
         </header>
     );
 };
---
+++ src/layouts/NavHeaderLayout.jsx
@@ -0,0 +1,7 @@
+import styles from "../assets/styles/layouts/navHeaderLayout.module.scss";
+
+export default function NavHeaderLayout() {
+    return (
+        <nav className={styles.navHeaderLayout}></nav>
+    );
+};
