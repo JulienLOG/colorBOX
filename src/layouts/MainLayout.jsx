fa7c635a306ffbb248a7d2c8eb19e923fe1b7985
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-07-31 01:54:59 +0200

    feat(layout): add MainLayout and debug flex behavior

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,7 @@
+import styles from "../assets/styles/layouts/mainLayout.module.scss";
+
+export default function MainLayout() {
+    return (
+        <main className={ styles.mainLayout }></main>
+    );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import styles from "../assets/styles/pages/homePage.module.scss";
 import HeaderLayout from "../layouts/HeaderLayout.jsx";
+import MainLayout from "../layouts/MainLayout.jsx";
 
 export default function HomePage() {
     return (
         <div className={styles.homePage}>
             <HeaderLayout></HeaderLayout>
+            <MainLayout></MainLayout>
         </div>
     );
 };  
