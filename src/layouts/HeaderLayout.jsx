97c9560f58c0924b3d07ac77512c27b27cb9bc67
JulienLOG 🌊 <mr.julien.pro@gmail.com>
2025-07-30 21:35:53 +0200

    feat(layout): add headerLayout.jsx and style test

---
+++ src/layouts/HeaderLayout.jsx
@@ -0,0 +1,7 @@
+import styles from "../assets/styles/layouts/headerLayout.module.scss";
+
+export default function HeaderLayout() { 
+    return (
+        <header className={styles.headerLayout}></header>
+    );
+};
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import styles from "../assets/styles/pages/homePage.module.scss";
+import HeaderLayout from "../layouts/HeaderLayout.jsx";
 
 export default function HomePage() {
     return (
-        <div className={styles.homePage}></div>
+        <div className={styles.homePage}>
+            <HeaderLayout>
+
+            </HeaderLayout>
+        </div>
     );
-};
+};  
